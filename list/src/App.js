58cc1fbb2f8f33ed8c3e6b17f7885a87e4a5f7d0
Rahul kumar barik <rahulbarik583@gmail.com>
2021-08-04 17:16:21 +0530

    pushing to github

---
+++ list/src/App.js
@@ -0,0 +1,57 @@
+import data from "./data.json";
+import Jobs from "./components/Jobs";
+import { useState } from "react";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import img from "./image/clogo.jpg";
+
+function App() {
+  const [filterKeywords, setfilterKeywords] = useState([]);
+
+  // const setSearchKeyword = (data) => {
+  //   setfilterKeywords(data);
+  // };
+
+  const addFilterKeywords = (data) => {
+    if (!filterKeywords.includes(data)) {
+      setfilterKeywords([...filterKeywords, data]);
+    }
+  };
+
+  const deleteKeyword = (data) => {
+    const newKeywords = filterKeywords.filter((key) => key !== data);
+    setfilterKeywords(newKeywords);
+  };
+
+  const clearAll = () => {
+    setfilterKeywords([]);
+  };
+
+  return (
+    <div>
+      <div className="header">
+            <div className="image">
+                <img  src={img} className="imgL" alt="nothing" />
+            </div>
+      </div>
+
+      {/* <Search setSearchKeyword={setSearchKeyword} /> */}
+
+      {filterKeywords.length > 0 && (
+        <Header
+          keywords={filterKeywords}
+          removeKeywords={deleteKeyword}
+          clearAll={clearAll}
+        />
+      )}
+
+      <Jobs
+        keywords={filterKeywords}
+        data={data}
+        setKeywords={addFilterKeywords}
+      />
+    </div>
+  );
+}
+
+export default App;
